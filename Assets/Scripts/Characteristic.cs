b0c2b06f7a3b22194344295978d5a0d4c3bf3298
Иван Арикайнен <ivans888888@mail.ru>
2019-01-24 13:23:49 +0300

    add GUI  edit

--- Assets/Scripts/Characteristic.cs
+++ Assets/Scripts/Characteristic.cs
@@ -22,6 +22,17 @@ public class Characteristic : MonoBehaviour {
     public Text _PhysicalDef;
     public Text _MagicDef;
 
+    [Header("GUI HP MP EXP NamePlayer")]
+    public Image _HpBar;
+    public Image _MpBar;
+    public Image _ExpBar;
+
+    public Text _NamePlayer;
+
+    [SerializeField] private string NamePlayer;
+    [SerializeField] private float NeedExp;
+    [SerializeField] private float RealExp;
+
     [Header("Basic Info")]
 
     [SerializeField] private string ClassPlayer;
@@ -31,21 +42,6 @@ public class Characteristic : MonoBehaviour {
     [SerializeField] private float MaxMp;
     [SerializeField] private float RealMp;
 
-    // Метод для проверки Hp на отрицаемость 
-    public float _Health
-    {
-        get { return RealHp; }
-        set
-        {
-            if (value < 0)
-            {
-                value = 0;
-            }
-        }
-    }
-
-
-
     [Header("Basic Characteristic")]
 
     [SerializeField] private int Strength;
@@ -54,13 +50,41 @@ public class Characteristic : MonoBehaviour {
     [SerializeField] private int Vitality;
 
     [Header("Characteristic Attack and Def")]
-    
+
     [SerializeField] private int MinPhysicalAttack;
     [SerializeField] private int MaxPhysicalAttack;
     [SerializeField] private int MinMagicAttack;
     [SerializeField] private int MaxMagicAttack;
     [SerializeField] private int PhysicalDef;
     [SerializeField] private int MagicDef;
+
+    // Метод для проверки отрицаемость 
+    //
+    public float _Health 
+    {
+        set
+        {
+            if (RealHp < 0)
+            {
+                RealHp = 0;
+            }
+        }
+        get { return RealHp; }
+    }
+
+    public float _Mana
+    {
+        set
+        {
+            if (RealMp < 0)
+            {
+                RealMp = 0;
+            }
+        }
+
+        get { return RealMp; }
+    }
+
 
 
     // Метод для открытия панели характеристик
@@ -72,6 +96,53 @@ public class Characteristic : MonoBehaviour {
 		return Open;
 	}
 	
+    // Метод для расчета HP MP EXP
+    //
+    public float CalculationHpMpExp(float RealNamber, float MaxNamber)
+    {
+        return RealNamber / MaxNamber;
+    }
+
+    // Метод для повышение лвл
+    //
+    public void LvlUp()
+    {
+        RealExp -= NeedExp;
+        NeedExp += 500;
+        
+        // Увеличение Basic характеристик
+        //
+        Lvl++;
+        MaxHP++;
+        MaxMp++;
+
+        Strength++;
+        Agiliy++;
+        Intelligence++;
+        Vitality++;
+
+        // Увеличение Attck и Def характеристик
+        //
+        MinPhysicalAttack = PhysiclAttck(MinPhysicalAttack, Strength, Agiliy); //
+        MaxPhysicalAttack = PhysiclAttck(MaxPhysicalAttack, Strength, Agiliy); // Обращение к методам для расчета
+        MinMagicAttack = MagicAttack(MinMagicAttack, Intelligence); // атаки в зависимости от Basic характеристик
+        MaxMagicAttack = MagicAttack(MaxMagicAttack, Intelligence); //
+        PhysicalDef++;
+        MagicDef++;
+    }
+
+    // Методы для расчета атаки в зависимости от добавленных характеристик
+    //
+    public int PhysiclAttck(int PhysicAttack, int Str, int Agl)
+    {
+        return 0;
+    }
+
+    public int MagicAttack(int MagicAttack, int Int)
+    {
+        return 0;
+    }
+
 
     void Start()
     {
@@ -95,13 +166,20 @@ public class Characteristic : MonoBehaviour {
         MagicDef = 150;
 
         _ClassPlayer.text = ClassPlayer;
+
+        RealHp = MaxHP;
+        RealMp = MaxMp;
+
+        NeedExp = 1000;
     }
 
     void LateUpdate()
     {
+        // Заполнение характеристик
+        //
         _Lvl.text = Lvl.ToString();
-        _HP.text = MaxHP.ToString();
-        _MP.text = MaxMp.ToString();
+        _HP.text = RealHp.ToString();
+        _MP.text = RealMp.ToString();
 
         _Strength.text = Strength.ToString();
         _Agility.text = Agiliy.ToString();
@@ -112,5 +190,16 @@ public class Characteristic : MonoBehaviour {
         _MaxAndMinMgAttack.text = MinMagicAttack.ToString() + " ~ " + MaxMagicAttack.ToString();
         _PhysicalDef.text = PhysicalDef.ToString();
         _MagicDef.text = MagicDef.ToString();
+        
+        // Заполнение GUI баров
+        //
+        _HpBar.fillAmount = CalculationHpMpExp(RealHp, MaxHP);
+        _MpBar.fillAmount = CalculationHpMpExp(RealMp, MaxMp);
+        _ExpBar.fillAmount = CalculationHpMpExp(RealExp, NeedExp) ;
+
+        if (RealExp >= NeedExp)
+        {
+            LvlUp();
+        }
     }
 }
